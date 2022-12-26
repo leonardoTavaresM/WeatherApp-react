05081c07b8f8bc64d3f508340ca3808992863425
Leonardo <leomaltadesa0712@gmail.com>
2022-12-25 21:10:20 -0300

    feat: adding api weatherApp, debounce hook

--- src/components/Seach/index.js
+++ src/components/Seach/index.js
@@ -1,8 +1,38 @@
-import React from "react";
-//teste squash
+import React, { useState, useEffect } from "react";
+import { useDebounce } from "../utils/functions";
+
+import * as S from "./style";
 
 const Search = () => {
-  return <input type="text" />;
+  const [data, setData] = useState({});
+  const [city, setCity] = useState("Turin");
+  const debounce = useDebounce(city);
+
+  const apiKey = "2fa3b6c245c46bd18becf7ea3958bdab";
+
+  function handleChange({ target }) {
+    console.log("target", target.value);
+    setCity(target.value);
+  }
+
+  useEffect(() => {
+    async function dataFetch() {
+      try {
+        let response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${debounce}&appid=${apiKey}`
+        );
+        let dataResponse = await response.json();
+        setData(dataResponse);
+      } catch (error) {}
+    }
+    dataFetch();
+  }, [debounce]);
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+    </div>
+  );
 };
 
 export default Search;
