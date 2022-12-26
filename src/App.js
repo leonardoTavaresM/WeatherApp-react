bb33687514ce73e4464f9a43a4a211bd8d54f677
Leonardo <leomaltadesa0712@gmail.com>
2022-12-25 21:12:46 -0300

    wip: adding router dom in the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// import { Routes } from "react-router-dom";
+import Search from "./components/Seach";
+
 function App() {
-  return <div className="App">teste</div>;
+  return (
+    <div className="App">
+      {/* <Routes /> */}
+
+      <h1>Teste</h1>
+      <Search />
+    </div>
+  );
 }
 
 export default App;
